b2aedb1f1fe06f0e37bd85dcf8619cf8e0a9c063
Milbad <badouxmilene@gmail.com>
2017-01-18 13:14:33 -0800

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -504,7 +504,9 @@ function updatePositions() {
   var items = document.getElementsByClassName('mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    console.log(items[i].basicLeft + 100 * phase + 'px');
+     items[i].style.transform = translateX(items[i].basicLeft + 100 * phase + 'px');
     
   }
   
